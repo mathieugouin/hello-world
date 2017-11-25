88e5b8f695b1837b40f1da78da80714e23c47e7c
Mathieu Gouin <mgouin@gmail.com>
2017-11-25 11:44:04 -0500

    print arg list

--- main.c
+++ main.c
@@ -7,8 +7,14 @@
  */
 int main(int argc, char * argv[])
 {
+  int i;
   static int x = 0;
   int y = 1;
+
+  for (i = 0; i < argc; i++)
+  {
+    printf("argv[%d] = %s\n", i, argv[i]);
+  }
 
   return EXIT_SUCCESS;
 }
