273bf6f0436600df6832f743f4ee67c72d72556e
Mathieu Gouin <mgouin@gmail.com>
2020-05-02 21:22:20 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,21 +3,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// branch-no-change-in-master
-// branch-no-change-in-master 2
-
-/**
- * Main function
- * change done in branch1
- * change 2 done in branch1
- */
 int main(int argc, char * argv[])
 {
-  int i;
-  for (i = 0; i < argc; i++)
-  {
-    printf("argv[%d] = %s\n", i, argv[i]);
-  }
+    int i;
+    for (i = 0; i < argc; i++)
+    {
+        printf("argv[%d] = %s\n", i, argv[i]);
+    }
 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
