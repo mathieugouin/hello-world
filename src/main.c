b3f2239d14803bac874321596b68e9b7e1f26269
Mathieu Gouin <mgouin@gmail.com>
2020-12-06 16:18:49 -0500

    added test strings v1

--- src/main.c
+++ src/main.c
@@ -5,6 +5,9 @@
 
 void test_func(void)
 {
+    const cpl_Uint8_t failString[5]          = "FAIL";
+    const cpl_Uint8_t connectedString[10]    = "CONNECTED";
+    const cpl_Uint8_t disconnectedString[13] = "DISCONNECTED";
 }
 
 int main(int argc, char * argv[])
